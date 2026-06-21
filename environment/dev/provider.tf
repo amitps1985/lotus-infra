00e126f9421302f27bfd023e909081aea441d712
amitps1985 <amit.prakash.singh2004@gmail.com>
2026-06-21 19:37:30 +0530

    Added Azure backend configuration

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -6,6 +6,12 @@ terraform {
     version = "4.40"
   }
 }
+ backend "azurerm" {
+    resource_group_name  = "tappu-rg"
+    storage_account_name = "stgtataaccount0101"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
 }
 provider "azurerm" {
   features { }
