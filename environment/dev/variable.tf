0443fc6e2e82d8325b1b9617049862828147bffb
amitps1985 <amit.prakash.singh2004@gmail.com>
2026-06-22 11:59:37 +0530

    Add terraform.tfvars

--- environment/dev/variable.tf
+++ environment/dev/variable.tf
@@ -1,8 +1,31 @@
-variable "rgs" {}
-variable "stgs" {}
-variable "vnets" {}
-variable "subnets" {}
-variable "nsgs" {}
-variable "pips" {}
-variable "vms" {}
-variable "bastions" {}
+variable "rgs" {
+  type = map(any)
+}
+
+variable "stgs" {
+  type = map(any)
+}
+
+variable "pips" {
+  type = map(any)
+}
+
+variable "vnets" {
+  type = map(any)
+}
+
+variable "subnets" {
+  type = map(any)
+}
+
+variable "nsgs" {
+  type = map(any)
+}
+
+variable "vms" {
+  type = map(any)
+}
+
+variable "bastions" {
+  type = map(any)
+}
